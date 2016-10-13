80acc133fe2657f37ab2d7205d31e185523be5e6
theaad94 <theaad94@gmail.com>
2016-10-13 22:46:06 +0200

    Lagt til funksjon for kart, og satt kordinatene til Beitostølen

--- booking.js
+++ booking.js
@@ -58,3 +58,12 @@
 
 
 		}
+
+	function myMap() {
+  		var mapCanvas = document.getElementById("map");
+  		var mapOptions = {
+    		center: new google.maps.LatLng(61.25, 8.917),
+    		zoom: 10
+  	}
+  		var map = new google.maps.Map(mapCanvas, mapOptions);
+}
